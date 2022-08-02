e484d44f9c76fca1b5d8d7bcfcfd9f8544febb62
Lachlan Jowett <lochyjowett1@gmail.com>
2022-08-02 21:10:30 +1000

    Dunno working on pathfind

--- Levels/Game.gd
+++ Levels/Game.gd
@@ -4,8 +4,15 @@ export(String) var path = "res://Menus/Completed.tscn"
 
 onready var exit = $Exit
 
+onready var player = $Player
+onready var nav = $Navigation2D
+
 func _ready():
 	exit.connect("on_exit", self, "Exit")
 
 func Exit():
 	Global.goto_scene(path)
+
+
+func _on_Timer_timeout():
+	get_tree().call_group("Enemy", 'get_target_path', player.global_position)
