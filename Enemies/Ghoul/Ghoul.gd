e22b36768aa44e70b0637e70f8e30baaab1e17a8
Lachlan Jowett <lochyjowett1@gmail.com>
2022-08-05 13:22:37 +1000

    Still working on the tutorial and the enemy pathfinding

--- Enemies/Ghoul/Ghoul.gd
+++ Enemies/Ghoul/Ghoul.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
 
-export(int) var MAX_SPEED = 80
+export(int) var MAX_SPEED = 1000
 export(int) var ACCELERATION = 450
 export(int) var FRICTION = 900
 
-onready var loc1 = $"Loc 1"
-onready var loc2 = $"Loc 2"
+onready var loc1 = $"Node2D/Loc 1"
+onready var loc2 = $"Node2D/Loc 2"
 onready var Sprite = $Sprite
 
 var extent = 1
@@ -16,21 +16,12 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	var loc = 1
 
-#	if loc2.global_position < Vector2(extent, extent):
-#		loc = 1
-#	if loc1.global_position < Vector2(extent, extent):
-#		loc = 2
 
-	if loc == 1 and loc1.global_position  <= Vector2(extent, extent):
-		input_vector.x = loc1.global_position.x
-		input_vector.y = loc1.global_position.y
-#	if loc == 2:
-#		input_vector.x = loc2.global_position.x
-#		input_vector.y = loc2.global_position.y
 
-	input_vector = input_vector.normalized()
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	if loc == 1:
+		velocity = velocity.move_toward(loc1.global_position.normalized() * MAX_SPEED, ACCELERATION * delta)
+	elif loc == 2:
+		velocity = velocity.move_toward(loc2.global_position.normalized() * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move()
--- Levels/Tutorial2.gd
+++ Levels/Tutorial2.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
-export(String) var path = "res://Levels/Tutorial2.tscn"
+export(String) var path = "res://Levels/Tutorial3.tscn"
 
 func _input(event):
 	if Input.is_action_pressed("ui_select"):
 		Global.goto_scene(path)
-
-
