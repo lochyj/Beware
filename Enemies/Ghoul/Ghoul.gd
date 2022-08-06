039af69f2e08fad9c9c357cea84ae3b3f1cb845a
Lachlan Jowett <lochyjowett1@gmail.com>
2022-08-06 17:42:10 +1000

    Fixed the enemy pathfinding

--- Enemies/Ghoul/Ghoul.gd
+++ Enemies/Ghoul/Ghoul.gd
@@ -1,31 +1,41 @@
 extends KinematicBody2D
 
-export(int) var MAX_SPEED = 1000
+export(int) var MAX_SPEED = 100
 export(int) var ACCELERATION = 450
 export(int) var FRICTION = 900
 
-onready var loc1 = $"Node2D/Loc 1"
-onready var loc2 = $"Node2D/Loc 2"
+onready var loc1 = $"Node2D/Location 1"
+onready var loc2 = $"Node2D/Location 2"
 onready var Sprite = $Sprite
 
 var extent = 1
 
 var velocity = Vector2.ZERO
+var loc = 1
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	var loc = 1
-
-
-
 	if loc == 1:
-		velocity = velocity.move_toward(loc1.global_position.normalized() * MAX_SPEED, ACCELERATION * delta)
-	elif loc == 2:
-		velocity = velocity.move_toward(loc2.global_position.normalized() * MAX_SPEED, ACCELERATION * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	move()
-
-func move():
+		goto(delta, loc1)
+	if loc == 2:
+		goto(delta, loc2)
 	velocity = move_and_slide(velocity)
+
+func goto(delta, location):
+	var direction = setDirection(location.global_position)
+	setVelocity(direction, delta)
 	Sprite.flip_h = velocity.x < 0
+
+func _on_Loc1_area_entered(area):
+	loc = 2
+	print("loc 1 hit")
+
+func _on_Loc2_area_entered(area):
+	loc = 1
+	print("loc 2 hit")
+
+func setVelocity(direction, delta):
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+
+func setDirection(dir):
+	return global_position.direction_to(dir)
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# Beware!
+
+## What is this?
+
+Beware is a simple game i created for my digital technology class that is meant to teach somone in pre-school to stay away from strangers.
+
+## How to build it?
+
+You need to have [Godot](https://godotengine.org/) installed.
+You then need to install the export templates for your platform.
+Then you can open the project in Godot and export it in you desired platform.
